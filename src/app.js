b093d7de332395528193668d995ed49de3a8b7da
Krishiv Periwal <krishivperiwal@gmail.com>
2026-06-16 16:39:17 +0530

    fix: resolve case-sensitive file path for deployment

--- src/app.js
+++ src/app.js
@@ -17,6 +17,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
+app.set('trust proxy', 1);
 const APP_PORT = process.env.PORT || 5000;
 const BASE_URL = process.env.BASE_URL || `http://localhost:${APP_PORT}`;
 
